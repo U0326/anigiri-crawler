fde451a7b1380d5e06c23bccfb4c681afd6e0c1f
Yuu Fujimura <koukokuiranaiyo@gmail.com>
2018-09-02 17:04:29 +0900

    tweet検索の際の制限を回避する機能を追加した。

--- anigiri/module/twitter/search_tweet.py
+++ anigiri/module/twitter/search_tweet.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import time
 from datetime import datetime, date, timedelta
 
 from pytz import timezone
@@ -13,7 +14,11 @@ logging.basicConfig(level=logging.INFO)
 class SearchTweet:
     SEARCH_URL = "https://api.twitter.com/1.1/search/tweets.json"
     TWEETS_PER_PAGE = 100
+    COOLDOWN_MINUTES_OF_SEARCH = 15
+    LIMITED_COUNT_OF_CONTINUOUSLY_SEARCH = 180
     NEGATIVE_WORDS = ['切った', '辞めた', 'やめた']
+
+    search_count = 0
 
     def __init__(self, date):
         self.today = date
@@ -32,15 +37,16 @@ class SearchTweet:
         while True:
             if 'max_id' in locals() : params['max_id'] = max_id
             try:
+                self.avoidLimited()
                 logger.info('Search query: ' + str(params))
                 response = twitter_api.get(self.SEARCH_URL, params = params)
                 response.raise_for_status()
                 timeline = json.loads(response.text)['statuses']
+                logger.info('Timeline size: ' + str(len(timeline)))
             except:
                 logger.exception('A HTTP request to take tweet is failed.')
                 raise
 
-            logger.info('Timeline size: ' + str(len(timeline)))
             for tweet in timeline:
                 logger.debug('tweet id: ' + str(tweet['id']))
                 logger.debug('tweet created at: ' + tweet['created_at'])
@@ -68,4 +74,10 @@ class SearchTweet:
                 'その内ネガティブなワードを含むものが' \
                 + str(negative_tweet_count) + '件でした。')
         return tweet_count, negative_tweet_count
+
+    def avoidLimited(self):
+        self.search_count += 1
+        if self.search_count >= self.LIMITED_COUNT_OF_CONTINUOUSLY_SEARCH:
+            time.sleep(self.COOLDOWN_MINUTES_OF_SEARCH * 60)
+            self.search_count = 0
 

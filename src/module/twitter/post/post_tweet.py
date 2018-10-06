52787f342bad2d46db0ee1a4b432b9cccdbbdae0
Yuu Fujimura <koukokuiranaiyo@gmail.com>
2018-10-06 18:35:30 +0900

    tweetを分割して投稿する機能を追加した。

--- src/module/twitter/post/post_tweet.py
+++ src/module/twitter/post/post_tweet.py
@@ -1,23 +1,46 @@
 import logging
 
-from .twitter_config import twitter_api
+from ..twitter_config import twitter_api
 
 logger = logging.getLogger(__name__)
 
 class PostTweet:
     POST_URL = 'https://api.twitter.com/1.1/statuses/update.json'
+    MAX_TWEET_LENGTH = 140
+    TWEET_SUFFIX = '(続く)...'
+    TWEET_PREFIX = '...(続き)'
+    HASHTAG = ' #あにぎり'
 
     def post_tweet(self, text):
-        params = {
-                'status': text
-                }
+        text_with_hashtag = text + self.HASHTAG
+        if self.is_need_split(text_with_hashtag):
+            self.do_post_splitted_tweet(text_with_hashtag)
+        else:
+            self.do_post_tweet(text_with_hashtag)
+
+    def is_need_split(self, text):
+        if len(text) > self.MAX_TWEET_LENGTH:
+            return True
+        return False
+
+    def do_post_splitted_tweet(self, text):
+        tweet_length = self.MAX_TWEET_LENGTH - len(self.TWEET_SUFFIX)
+        self.do_post_tweet(text[0:tweet_length] + self.TWEET_SUFFIX)
+
+        # 7文字のsuffixを挿入した後に分割を行う為、
+        # ↑hashtagの途中で文字数制限になっても、hashtag全体が次のツイートとして扱われる。
+        remaining_text = self.TWEET_PREFIX + text[tweet_length:len(text)]
+        if self.is_need_split(remaining_text):
+            self.do_post_splitted_tweet(remaining_text)
+        else:
+            self.do_post_tweet(remaining_text)
+
+    def do_post_tweet(self, text):
         try:
-            response = twitter_api.post(self.POST_URL, params = params)
+            logger.info('Sending tweet content is "' + text + "'.")
+            response = twitter_api.post(self.POST_URL, params = {'status' : text})
             response.raise_for_status()
         except:
             logger.exception('A HTTP request to post tweet is failed.')
             raise
 
-if __name__ == '__main__':
-    post_tweet = PostTweet()
-    post_tweet.post_tweet('テスト投稿')
